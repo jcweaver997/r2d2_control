d857f15604e5e54708aea5ad92c7878fc7230cc3
jcweaver997 <jcweaver997@gmail.com>
2019-10-26 08:00:14 -0400

    asd

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,58 @@
 mod r2d2_connection;
-use r2d2_connection::R2D2Connection;
+use r2d2_connection::*;
+use std::mem::transmute;
+use std::io;
+use std::io::prelude::*;
+use gilrs::*;
+use parking_lot::RwLock;
+use std::sync::Arc;
+use std::thread;
 
-static R2D2_HOSTNAME:&str = "jcwork.local";
+static R2D2_HOSTNAME:&str = "jc-desktop.local";
 static R2D2_PORT:u16 = 1296;
 
+fn recv(code:u8,values:&[u8;4]){
+    println!("got message {},{:?}",code,values);
+}
+
 fn main() {
-    let connection = R2D2Connection::new(R2D2_HOSTNAME,R2D2_PORT);
+    let mut connection = R2D2Connection::new();
+    connection.connect(R2D2_HOSTNAME,R2D2_PORT,ConnectionType::Client);
+    let mut server = R2D2Connection::new();
+    server.connect("0.0.0.0",1296,ConnectionType::Server);
+    server.set_recv_fn(recv);
+    connection.add(2,unsafe{&transmute(5_u32)});
+    connection.add(3,unsafe{&transmute(5_u32)});
+    connection.add(4,unsafe{&transmute(5_u32)});
+    connection.send();
+
+
+
+    let mut gilrs = Gilrs::new().unwrap();
+    println!("Pick a controller:");
+    let mut gamepadId = None;
+    for (id, gamepad) in gilrs.gamepads(){
+        println!("{}. {}", id,gamepad.name());
+    }
+    print!("Selection: ");
+    io::stdout().flush().expect("failed to flush stdout");
+    let input = io::stdin().lock().lines().next().expect("EOF").expect("?");
+    let index: u32 = input.parse().expect("Not a number");
+    for (id, gp) in gilrs.gamepads(){
+        if id.to_string() == index.to_string(){
+            gamepadId = Some(id);
+        }
+    }
+
+    loop{
+        while gilrs.next_event().is_some() {}
+
+        if let Some(id) = gamepadId{
+            println!("sd {:?}",gilrs.gamepad(id).is_pressed(Button::DPadRight));
+        }
+
+        std::thread::sleep(std::time::Duration::from_millis(50));
+    }
+
 
 }
--- src/r2d2_connection.rs
+++ src/r2d2_connection.rs
@@ -1,38 +1,88 @@
-use parking_lot::{Mutex,RwLock};
+use parking_lot::{RwLock};
 use std::sync::Arc;
-use std::net::{UdpSocket, Ipv4Addr};
+use std::net::{UdpSocket};
+use std::thread;
 
-struct R2D2ConnectionData{
-    port:Arc<Mutex<u16>>,
-    hostname:Arc<Mutex<String>>,
-    socket:Arc<RwLock<UdpSocket>>
+#[derive(PartialEq,Copy,Clone)]
+pub enum ConnectionType{
+    Client,Server,None
 }
 
 pub struct R2D2Connection{
-    connection_data:Option<R2D2ConnectionData>
+    socket:Option<Arc<RwLock<UdpSocket>>>,
+    next_packet:(Vec<u8>,Vec<u8>),
+    recv_fn:Arc<RwLock<Option<fn(u8,&[u8;4])>>>,
+    connection_type:ConnectionType
 }
 
 impl R2D2Connection{
-    pub fn new(hostname:&str,port:u16) -> R2D2Connection{
-        println!("started discovery for {}",hostname);
-
-
-
-
-        R2D2Connection{connection_data:None}
+    pub fn new() -> R2D2Connection{
+        R2D2Connection{
+            socket:None,
+            next_packet:(Vec::new(),Vec::new()),
+            recv_fn:Arc::new(RwLock::new(None)),
+            connection_type:ConnectionType::None
+        }
     }
 
-    fn get_ip_from_mdns(service_name:&str) -> Option<Ipv4Addr>{
-        let mut discovery = mdns::discover::all(hostname).expect("mDNS failure");
-        discovery = discovery.timeout(std::time::Duration::from_secs(5));
-        let result = discovery.next();
-        if !result.is_some(){return None;}
-        let ip = result.unwrap().records().find(|x| if)
-        println!("mDNS result: {:?}",result);
-        return None;
+    pub fn connect(&mut self, hostname: &str, port: u16, con_type: ConnectionType){
+            self.connection_type = con_type;
+        if con_type == ConnectionType::Client{
+            self.socket = Some(Arc::new(RwLock::new(UdpSocket::bind("0.0.0.0:0").expect("Failed to bind UDP socket"))));
+            self.socket.as_ref().unwrap().write().connect(format!("{}:{}",hostname,port)).expect("Failed to connect UDP socket");
+            self.start_listener();
+        }else if con_type == ConnectionType::Server{
+            self.socket = Some(Arc::new(RwLock::new(UdpSocket::bind(format!("0.0.0.0:{}",port)).expect("Failed to bind UDP socket"))));
+            self.start_listener();
+        }
     }
 
-    pub fn connect(){
+    pub fn set_recv_fn(&self, recv_fn:fn(u8,&[u8;4])){
+        *self.recv_fn.write() = Some(recv_fn);
+    }
 
+    fn start_listener(&self){
+        let socket = self.socket.as_ref().unwrap().clone();
+        let recv_fn = self.recv_fn.clone();
+        let connection_type = self.connection_type;
+        thread::spawn(move ||{
+            loop{
+                let mut buffer = [0_u8;100];
+                let (count, remote) = socket.read().recv_from(&mut buffer).expect("Failed to read from socket");
+                if connection_type == ConnectionType::Server{socket.read().connect(remote).expect("Failed to connect socket")};
+                if let Some(recv) = *recv_fn.read(){
+                    if count%5 == 0{
+                        let mut value = [0_u8;4];
+                        for i in 0..count/5{
+                            for j in 0..4{
+                                value[j] = buffer[i*5+j+1];
+                            }
+                            recv(buffer[i*5],&value);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    pub fn add(&mut self, code:u8, value:&[u8;4]){
+        self.next_packet.0.push(code.clone());
+        self.next_packet.1.extend(value);
+    }
+
+    pub fn send(&mut self){
+        if self.next_packet.0.len() == 0{return;}
+        if let Some(socket) = &self.socket{
+            let mut buffer = Vec::new();
+            for i in 0..self.next_packet.0.len(){
+                buffer.push(self.next_packet.0[i]);
+                for j in 0..4{
+                    buffer.push(self.next_packet.1[i*4+j]);
+                }
+            }
+            socket.read().send(&buffer).expect("Failed to send packet");
+            self.next_packet.0.clear();
+            self.next_packet.1.clear();
+        }
     }
 }
